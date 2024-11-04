522297b8791a3822cfe3a0a06cd7887c891cfe37
arne314 <73391160+arne314@users.noreply.github.com>
2024-11-04 19:22:02 +0100

    fix: only import modules on setup call

--- lua/typstar/init.lua
+++ lua/typstar/init.lua
@@ -1,13 +1,12 @@
 local M = {}
 
 local config = require('typstar.config')
-local excalidraw = require('typstar.excalidraw')
-
-vim.api.nvim_create_user_command('TypstarInsertExcalidraw', excalidraw.insert_drawing, {})
-vim.api.nvim_create_user_command('TypstarOpenExcalidraw', excalidraw.open_drawing, {})
 
 M.setup = function(args)
     config.merge_config(args)
+    local excalidraw = require('typstar.excalidraw')
+    vim.api.nvim_create_user_command('TypstarInsertExcalidraw', excalidraw.insert_drawing, {})
+    vim.api.nvim_create_user_command('TypstarOpenExcalidraw', excalidraw.open_drawing, {})
 end
 
 return M
